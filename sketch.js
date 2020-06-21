4d4bd10443252569d592f459ae26d5a5a12acd3d
zoyron <sgrarora3@gmail.com>
2020-06-21 19:09:07 +0530

    spooky experiment on perlin noise

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
 let start = 0;
-let inc = 0.01;
+let inc = 0.005;
 function setup() {
-  createCanvas(600, 600);
-  background(51);
+  createCanvas(windowWidth, windowHeight);
+  background("f1f1f1");
 }
 
-function draw() {}
+function draw() {
+  let xoff = start;
+  // stroke(random(200), random(200), random(200));
+  noFill();
+  // fill(random(200), random(200), random(200));
+  stroke(0, (start * 5) % 255, (start * 10) % 255);
+  // fill((start * 10) % 100, (start * 20) % 100, (start * 30) % 100);
+  beginShape();
+  for (let x = 0; x < width; x++) {
+    let y = noise(xoff) * height;
+    point(x, y);
+    xoff += inc;
+  }
+  endShape();
+  start += inc;
+  // noLoop();
+}
