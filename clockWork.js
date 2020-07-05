8a1ec2d227a917edbe75f3bd959481bf4abb150d
zoyron <sgrarora3@gmail.com>
2020-07-05 19:02:08 +0530

    minor work with rotation done

--- clockWork.js
+++ clockWork.js
@@ -1,7 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
   background(0);
+}
+let x = 0;
+let y = 0;
+function draw() {
+  translate(width / 2, height / 2);
+  rotate(x);
+  stroke(200, 75);
+  line(-x * 8, x, x * 5, x);
+  x += 0.125;
 }
