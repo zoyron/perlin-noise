b09dc0f51d24727383b1667f0b32d8d4d63e74bd
zoyron <sgrarora3@gmail.com>
2020-07-05 20:18:30 +0530

    another color experiment with perlin noise

--- clockWork.js
+++ clockWork.js
@@ -8,8 +8,8 @@ let y = 0;
 function draw() {
   translate(width / 2, height / 2);
   rotate(x);
-  // stroke(map(noise(xoff), 0, 1, 0, 55), 75);
-  stroke(5, 75);
+  stroke(map(noise(xoff), 0, 1, 0, 55), 75);
+  // stroke(5, 75);
   line(-x * 8, x, x * 5, x);
   x += 0.175;
   xoff += 0.01;
