8d231367b9ef857955017893e6b1bb45c880c5d3
zoyron <sgrarora3@gmail.com>
2020-07-05 20:16:43 +0530

    experiment with the color using perlin noise

--- clockWork.js
+++ clockWork.js
@@ -1,13 +1,30 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background("#dadada");
 }
+let xoff = 0;
 let x = 0;
 let y = 0;
 function draw() {
   translate(width / 2, height / 2);
   rotate(x);
-  stroke(200, 75);
+  // stroke(map(noise(xoff), 0, 1, 0, 55), 75);
+  stroke(5, 75);
   line(-x * 8, x, x * 5, x);
-  x += 0.125;
+  x += 0.175;
+  xoff += 0.01;
 }
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   background(0);
+// }
+// let x = 0;
+// let y = 0;
+// function draw() {
+//   translate(width / 2, height / 2);
+//   rotate(x);
+//   stroke(200, 75);
+//   line(-x * 8, x, x * 5, x);
+//   x += 0.125;
+// }
